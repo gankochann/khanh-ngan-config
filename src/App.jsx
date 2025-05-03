9faae8802181b945ebed25c530142f1caad296b4
gankochann <phamngan0208hy@gmail.com>
2025-05-04 00:24:00 +0900

    nop bai tap

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import CounText from '../SS04_bai_tap_01/CounText'
+import RandomNumber from '../SS04_bai_tap_01/RandomNumber'
+import ToggleInfo from '../SS04_bai_tap_01/ToggleInfo'
+import UseForm from '../SS04_bai_tap_01/UseForm'
+import UserCars from '../SS04_bai_tap_01/UserCars'
+import './App.css'
+import BgColor from './SS05_bai_tap/BgColor'
+import LoggerCounter from './SS05_bai_tap/LoggerCounter'
+import Manageform from './SS05_bai_tap/Manageform'
+import Watch from './SS05_bai_tap/Watch'
+import Demo_Antd_Design from './SS7_UI_Framework/Demo_Antd_Design'
+import DemoReactBootstrap from './SS7_UI_Framework/DemoReactBootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Square_Tailwind from './SS7_UI_Framework/Square_Tailwind'
+import FlexTailwind from './SS7_UI_Framework/FlexTailwind'
+import Table_Tailwind from './SS7_UI_Framework/Table_Tailwind'
+
+function App() {
+ 
+
+  return (
+    <>
+      {/* <UserCars></UserCars> */}
+      {/* <ToggleInfo></ToggleInfo> */}
+      {/* <UseForm></UseForm> */}
+      {/* <CounText></CounText> */}
+      {/* <RandomNumber></RandomNumber> */}
+      {/* <BgColor></BgColor> */}
+      {/* <LoggerCounter></LoggerCounter> */}
+      {/* <Manageform></Manageform> */}
+      {/* <Watch></Watch> */}
+      {/* <DemoReactBootstrap></DemoReactBootstrap> */}
+      {/* <Demo_Antd_Design></Demo_Antd_Design> */}
+      {/* <Square_Tailwind></Square_Tailwind> */}
+      <FlexTailwind></FlexTailwind>
+      {/* <Table_Tailwind></Table_Tailwind> */}
+
+    </>
+  )
+}
+
+export default App
---
+++ src/SS7_UI_Framework/Demo_Antd_Design.jsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd'
+import React from 'react'
+
+export default function Demo_Antd_Design() {
+  return (
+    <>
+    <Button type='primary'>Primary</Button>
+    </>
+  )
+}
